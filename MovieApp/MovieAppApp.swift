4dca111cd8e39d57dbee545b49dc401532cc83eb
ogbodotg <ogbodosolution@gmail.com>
2026-02-18 06:12:55 +0100

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by OGBODO on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
